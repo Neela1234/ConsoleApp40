83665dfcfd8da77a810cf48d0908ef0333b8159c
home <home@WINDOWS-PQ21EPG>
2022-01-12 12:30:35 +0530

    Add project files.

---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp40
+{
+   public  class Program
+    {
+        static void Main(string[] args)
+        {
+           using (var context = new EmployeeContext())
+            {
+                List<EmployeeDetails> employees = context.EmployeeDetails.ToList();
+                foreach(var employeeDetails in employees)
+                {
+                    Console.WriteLine(employeeDetails.Salary);
+                }
+            }
+        }
+    }
+}
